9b1417d4fef1b24f1cc625dbc33cac512d0d57f9
Coleman T <colemantung@users.noreply.github.com>
2022-08-27 19:02:59 -0700

    Init repo with Maven
    Init repo with Maven init command:
    "mvn archetype:generate -DgroupId=com.colemantung.app -DartifactId=flow-aggregation -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false"

---
+++ flow-aggregation/src/main/java/com/colemantung/app/App.java
@@ -0,0 +1,13 @@
+package com.colemantung.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
